e57d1b2be4f00358e582b57a7296149c5b225c88
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-06-12 17:51:47 +0500

    Updated App logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@
 """
 Created on Sat Jan 22 08:24:00 2022
 @author: MuhammadAli
-@org: Digital Product School
-@project: AI-MakerSpace
+@org: Deep Learning Lab
+@hackathon: Swiss AI
 """
 
 from pycaret.regression import load_model, predict_model
@@ -15,82 +15,31 @@ def predict_rating(model, df):
     
     predictions_data = predict_model(estimator = model, data = df)
     
-    return predictions_data['Label'][0]
+    return predictions_data[['Label', 'Score']]
     
-model = load_model('RF_Model_V1')
+model = load_model('model/random_forest_v1')
 
-
-st.title('Loan Classification Web App')
-st.write('This is a web app to classify whether a person is eligible for a loan or not based on\
-        several features that you can see in the sidebar. Please adjust the\
-        value of each feature. After that, click on the Predict button at the bottom to\
+st.title('Illicit Transaction Classification Web App')
+st.write('This is a web app to classify whether a Bitcoin transaction is legal or not based on\
+        several features provided by Elliptic Ltd. is a London-based blockchain analysis provider. Please upload the\
+        csv file with a transaction record that you want to test. After that, click on the Predict button at the bottom to\
         see the prediction.')
 
-                        
-Age = int(st.sidebar.number_input(label = 'Age', value = 37))
-
-Experience = int(st.sidebar.number_input(label = 'Experience', value = 13))
-
-Income = int(st.sidebar.number_input(label = 'Income', value = 29))
-
-Family = st.sidebar.selectbox(
-    "Family",
-    (1,2,3,4)
-)
-
-CCAvg = float(st.sidebar.number_input(label = 'CCAvg', value = 1.6))
-
-CDAccount = st.sidebar.selectbox(
-    "CDAccount",
-    (0,1)
-)
-
-SecuritiesAccount = st.sidebar.selectbox(
-    "SecuritiesAccount",
-    (0,1)
-)
-
-Education = st.sidebar.selectbox(
-    "Education",
-    (1,2,3)
-)
-
-Mortgage = int(st.sidebar.number_input(label = 'Mortgage', value = 155))
-
-Online = st.sidebar.selectbox(
-    "Online",
-    (0,1)
-)
-
-CreditCard = st.sidebar.selectbox(
-    "CreditCard",
-    (0,1)
-)
-
-features={
-    'Age':Age, 
-    'Experience':Experience,
-    'Income':Income,
-    'Family':Family,
-    'CCAvg': CCAvg,
-    'Education':Education,	
-    'Mortgage':Mortgage,
-    'SecuritiesAccount':SecuritiesAccount,	
-    'CDAccount':CDAccount,	
-    'Online':Online,
-    'CreditCard':CreditCard}
-
-features_df  = pd.DataFrame([features])
+# file(s) part
+features_df=None
+uploaded_file = st.file_uploader("Upload your CSV file")
+if uploaded_file is not None:
+  features_df = pd.read_csv(uploaded_file)
 
 st.table(features_df)  
 
 if st.button('Predict'):
     
-    prediction = predict_rating(model, features_df)
-
-    if prediction == 1:
-        prediction = "Accepted"
-    else:
-        prediction = "Rejected"
-    
-    st.write(' Based on feature values, the Personal Loan Application is '+ prediction)
+    if dataframe is None:
+    st.error("Please Upload Your CSV")
+  else:
+    with st.spinner('File is Processing...'):
+      output = predict_rating(model, features_df)
+      csv = convert_df(output)
+      st.download_button(label="Download Generated Output CSV", file_name='output_data.csv', data=csv, mime='text/csv')
+      st.success('And your file is ready!')
